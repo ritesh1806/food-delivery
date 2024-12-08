a7b67c8cbed8a8e9e8454b31e35b07f69f4cf897
ritesh1806 <riteshagarwal.1318@gmail.com>
2024-12-09 01:52:28 +0530

    created food delivery app

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1 className="logo">QuickBite</h1>
+      <h3>Admin Panel</h3>
+    </div>
+  );
+};
+
+export default Navbar;
